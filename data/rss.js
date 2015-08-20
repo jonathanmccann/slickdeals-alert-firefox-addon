af9973de16be6bdf9bc4b42aa022e0dff45c60e4
Jonathan McCann <jonathan.mccann@liferay.com>
2015-08-20 10:42:52 -0700

    Truncate the deal's title to be able to space the panel properly

--- data/rss.js
+++ data/rss.js
@@ -3,11 +3,21 @@ var previousDealTitle;
 setInterval(function() {
 	$.get('http://slickdeals.net/newsearch.php?mode=frontpage&searcharea=deals&searchin=first&rss=1', function (data) {
 		var rssItem = $(data).find("item:first");
+		var dealTitle = truncateTitle(rssItem.find("title").text());
 
-		if (previousDealTitle != rssItem.find("title").text()) {
-			self.port.emit("notify", rssItem.find("title").text(), rssItem.find("link").text());
+		if (previousDealTitle != dealTitle) {
+			self.port.emit("notify", dealTitle, rssItem.find("link").text());
 
-			previousDealTitle = rssItem.find("title").text();
+			previousDealTitle = dealTitle;
 		}
 });
 }, 10000);
+
+function truncateTitle(dealTitle){
+	if (dealTitle.length > 75) {
+		return dealTitle.substring(0, 75) + '...';
+	}
+	else {
+		return dealTitle;
+	}
+};
