8fe052a21a1a0d8c5190ea4c0b3a9723799e6297
Jonathan McCann <jonathan.mccann@liferay.com>
2015-08-20 08:11:10 -0700

    Store the previous deal's title to not alert the user of the same deal multiple times.

--- data/rss.js
+++ data/rss.js
@@ -1,9 +1,16 @@
+var previousDealTitle;
+
 setInterval(function() {
 	$.get('http://slickdeals.net/newsearch.php?mode=frontpage&searcharea=deals&searchin=first&rss=1', function (data) {
 		var rssItem = $(data).find("item:first");
-		console.log("title - " + rssItem.find("title").text());
-		console.log("href - " + rssItem.find("link").text());
 
-		self.port.emit("notify", rssItem.find("title").text(), rssItem.find("link").text());
+		if (previousDealTitle != rssItem.find("title").text()) {
+			console.log("title - " + rssItem.find("title").text());
+			console.log("href - " + rssItem.find("link").text());
+
+			self.port.emit("notify", rssItem.find("title").text(), rssItem.find("link").text());
+
+			previousDealTitle = rssItem.find("title").text();
+		}
 });
 }, 10000);
