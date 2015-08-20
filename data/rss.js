0ec8540c99d5f9668e1362d5ea76ef5ca852926f
Jonathan McCann <jonathan.mccann@liferay.com>
2015-08-20 15:37:49 -0700

    Use ajax instead of JQuery to get the URL in order to bypass the cache

--- data/rss.js
+++ data/rss.js
@@ -2,16 +2,20 @@ var previousDealTitle;
 
 // Query Slickdeals for its Frontpage RSS feed and determine if there is a new deal to notify the user of
 setInterval(function() {
-	$.get('http://slickdeals.net/newsearch.php?mode=frontpage&searcharea=deals&searchin=first&rss=1', function (data) {
-		var rssItem = $(data).find("item:first");
-		var dealTitle = truncateTitle(rssItem.find("title").text());
+	$.ajax({
+		url: "http://slickdeals.net/newsearch.php?mode=frontpage&searcharea=deals&searchin=first&rss=1",
+		cache: false,
+		success: function (data) {
+			var rssItem = $(data).find("item:first");
+			var dealTitle = truncateTitle(rssItem.find("title").text());
 
-		if (previousDealTitle != dealTitle) {
-			self.port.emit("newDeal", dealTitle, rssItem.find("link").text());
+			if (previousDealTitle != dealTitle) {
+				self.port.emit("newDeal", dealTitle, rssItem.find("link").text());
 
-			previousDealTitle = dealTitle;
+				previousDealTitle = dealTitle;
+			}
 		}
-});
+	});
 }, 10000);
 
 // Truncate the deal's title to have a uniform panel size
